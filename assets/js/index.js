3a04f48893aba0b301d7e419d2e9006ab82d1dc7
yoan parede <parede.yoan.amcs@gmail.com>
2020-11-01 16:02:38 +0100

    create site

---
+++ assets/js/index.js
@@ -0,0 +1,80 @@
+// recuperation du contener div pour afficher le resultat
+let resultDiv = document.getElementById('result-input');
+let input = document.getElementById('input-bunker');
+let b5 = {
+  name:"Bunker 1",
+  position:"B5",
+  code:'87624851'
+}
+let b7 = {
+  name:"Bunker 2",
+  position: "B7",
+  code:'97264138'
+}
+let f8 = {
+  name:"Bunker 3",
+  position: "F8",
+  code:'60274513'
+}
+let f4 = {
+  name:"Cabane 1",
+  position:'F4',
+  code:'27495810'
+}
+let h8 = {
+  name:"Cabane 2",
+  position: "H8",
+  code:'72948531'
+}
+let h6 = {
+  name:'Cabane',
+  position: "H6",
+  code:'49285463'
+}
+input.addEventListener('input', function submit(e){
+  if(e.target.value == "b5" || e.target.value == "B5"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du "+ b5.name +" placer en "+ b5.position+ " est: <span class='span-result'>"+  b5.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }else if(e.target.value == "b7" || e.target.value == "B7"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du " + b7.name + "placer en " + b7.position+ " est: <span class='span-result'>"+  b7.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }else if(e.target.value == "f8" || e.target.value == "F8"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du " + f8.name + " placer en " + f8.position+ " est: <span class='span-result'>"+  f8.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }else if(e.target.value == "f4" || e.target.value == "F4"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du " + f4.name + " placer en " + f4.position+ " est: <span class='span-result'>"+  f4.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }else if(e.target.value == "h8" || e.target.value == "H8"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du " + h8.name + " placer en " + h8.position+ " est: <span class='span-result'>"+  h8.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }else if(e.target.value == "h6" || e.target.value == "H6"){
+    let result = document.createElement('p');
+    result.innerHTML = "<p class='result'>Le code du " + h6.name + " placer en " + h6.position+ " est: <span class='span-result'>"+  h6.code+"</span></p>";
+    resultDiv.appendChild(result)
+    console.log(result)
+  }
+});
+
+// fonction qui affiche / et cache un menu deroulant par exemble //
+
+/*let btn = document.getElementById('btn');
+btn.addEventListener('click', function visibleHidde(){
+    var content = document.getElementById("content");
+    var contentDisplay = content.style.display;
+    if(contentDisplay == "none"){
+      
+      content.style.display = "block";
+    }else{
+      content.style.display ="none";
+  }
+})*/
